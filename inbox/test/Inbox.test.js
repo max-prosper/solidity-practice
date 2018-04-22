34dfd5cb34ace1c43ae63ec77f4a0c79969fec92
Max Prosper <maxim.yaaman@gmail.com>
2018-04-22 17:31:54 +0300

    Inbox: Test for message update

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,7 +2,9 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require("../compile");
 
 let accounts;
@@ -15,10 +17,23 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ["Hi there!"] })
     .send({ from: accounts[0], gas: "1000000" });
+
+  inbox.setProvider(provider);
 });
 
 describe("Inbox", () => {
   it("deploys a contract", async () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("Hello there!").send({ from: accounts[0] });
+    const newMessage = await inbox.methods.message().call();
+    assert.equal(newMessage, "Hello there!");
   });
 });
