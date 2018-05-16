5b938200916579c8f258cd5d8e99b5cec274ac2f
Max Prosper <maxim.yaaman@gmail.com>
2018-05-16 21:54:06 +0300

    Kickstart: Contract redeplyed with new functions

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0xb1f0B2114F5d232Bf8627BddaC90A4C2b51B4535"
+  "0x4e47B57f4639A46c731a9A557ca2c12c8b0f405d"
 );
 
 export default instance;
