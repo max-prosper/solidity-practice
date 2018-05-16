b1bd504d33cb04993ec6581f77affe420b2210ca
Max Prosper <maxim.yaaman@gmail.com>
2018-05-16 20:35:14 +0300

    Kickstart: Basic UI for index and new campaign routes

---
+++ kickstart/.next/dist/components/Header.js
@@ -0,0 +1,43 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _semanticUiReact = require("semantic-ui-react");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/maxprosper/Development/solidity-practice/kickstart/components/Header.js";
+
+exports.default = function () {
+    return _react2.default.createElement(_semanticUiReact.Menu, { style: { marginTop: "10px" }, __source: {
+            fileName: _jsxFileName,
+            lineNumber: 6
+        }
+    }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 7
+        }
+    }, "CrowdCoin"), _react2.default.createElement(_semanticUiReact.Menu.Menu, { position: "right", __source: {
+            fileName: _jsxFileName,
+            lineNumber: 9
+        }
+    }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 10
+        }
+    }, "Campaign"), _react2.default.createElement(_semanticUiReact.Menu.Item, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 12
+        }
+    }, "+")));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvSGVhZGVyLmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiTWVudSIsIm1hcmdpblRvcCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQVMsQUFFVDs7Ozs7O2tCQUFlLFlBQU0sQUFDakI7MkJBQ0ksQUFBQyx1Q0FBSyxPQUFPLEVBQUUsV0FBZixBQUFhLEFBQWE7c0JBQTFCO3dCQUFBLEFBQ0k7QUFESjtLQUFBLGtCQUNLLGNBQUQsc0JBQUEsQUFBTTs7c0JBQU47d0JBQUE7QUFBQTtBQUFBLE9BREosQUFDSSxBQUVBLDhCQUFDLGNBQUQsc0JBQUEsQUFBTSxRQUFLLFVBQVgsQUFBb0I7c0JBQXBCO3dCQUFBLEFBQ0k7QUFESjt1QkFDSyxjQUFELHNCQUFBLEFBQU07O3NCQUFOO3dCQUFBO0FBQUE7QUFBQSxPQURKLEFBQ0ksQUFFQSw2QkFBQyxjQUFELHNCQUFBLEFBQU07O3NCQUFOO3dCQUFBO0FBQUE7QUFBQSxPQVBaLEFBQ0ksQUFHSSxBQUdJLEFBSWY7QUFaRCIsImZpbGUiOiJIZWFkZXIuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL21heHByb3NwZXIvRGV2ZWxvcG1lbnQvc29saWRpdHktcHJhY3RpY2Uva2lja3N0YXJ0In0=
